7dadd0e48aea017339d1f7b61dea0c1e6604ec43
chungchungdev <chungchung.dev@gmail.com>
2024-05-13 07:02:43 +0800

    wrapping okio operation with use{}

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,55 +4,61 @@ import okio.FileSystem
 import okio.Path
 import okio.Path.Companion.toPath
 import okio.buffer
+import okio.use
 import java.nio.file.Files
-import java.nio.file.Paths
 
 
 fun main() {
-    val outputDir = "src/main/kotlin/ggendata/modified/"
-    val path = "src/main/kotlin/ggendata/MachineSpecList.tbl".toPath(normalize = true)
-    decodeToFile(path, outputDir)
+    val decodeOutputDir = "src/main/kotlin/ggendata/decoded/"
+    val path = "src/main/kotlin/ggendata/MachineSpecList.tbl".toPath()
+    decodeToFile(path, decodeOutputDir)
+    val editedPath = "src/main/kotlin/ggendata/edited/MachineSpecList.tbl".toPath()
+    val repackOutputDir = "src/main/kotlin/ggendata/repack/".toPath()
+    encodeFromFile(path, editedPath, repackOutputDir)
 }
 
 fun decodeToFile(src: Path, destDir: String) {
-    val filename = src.name.split(".")[0]
-    val fileExtension = ".txt"
-    val rawBytes = FileSystem.SYSTEM.source(src).buffer().readByteArray()
-    val startIndex = findContentStartIndex(rawBytes)
-    val content = rawBytes.copyOfRange(startIndex, rawBytes.size)
-    val line = mutableListOf<Byte>()
-    val contentByLine = StringBuilder()
-
-    for (i in content.indices) {
-        if (content[i] != 0.toByte()) {
-            line.add(content[i])
+    val filename = src.name
+    FileSystem.SYSTEM.source(src).buffer().use { source ->
+        val rawBytes = source.readByteArray()
+        val startIndex = findContentStartIndex(rawBytes)
+        val content = rawBytes.copyOfRange(startIndex, rawBytes.size)
+        val line = mutableListOf<Byte>()
+        val contentByLine = StringBuilder()
+        for (i in content.indices) {
+            if (content[i] != 0.toByte()) {
+                line.add(content[i])
+            }
+            if (content[i] == 0.toByte()) {
+                contentByLine
+                    .append(line.toByteArray().toString(Charsets.UTF_8))
+                    .append("\n")
+                line.clear()
+            }
+            if (i == content.size - 1) {
+                contentByLine.setLength(contentByLine.length - 1)
+            }
         }
-        if (content[i] == 0.toByte()) {
-            contentByLine
-                .append(line.toByteArray().toString(Charsets.UTF_8))
-                .append("\n")
-            line.clear()
-        }
-        if (i == content.size - 1) {
-            contentByLine.setLength(contentByLine.length - 1)
+        FileSystem.SYSTEM.sink("$destDir$filename".toPath()).buffer().use { sink ->
+            sink.writeUtf8(contentByLine.toString())
         }
     }
-    val finalContent = contentByLine.toString()
-        .also { println("final content char count: ${it.length}") }
-
-    FileSystem.SYSTEM.sink("$destDir$filename-okio$fileExtension".toPath()).buffer().writeUtf8(finalContent)
-    Files.writeString("$destDir$filename-nio$fileExtension".toPath().toNioPath(), finalContent, Charsets.UTF_8)
-
-    val okioOutput = "src/main/kotlin/ggendata/modified/MachineSpecList-okio.txt".toPath()
-        .let { FileSystem.SYSTEM.source(it).buffer().readUtf8() }
-    val nioOutput = Paths.get("src/main/kotlin/ggendata/modified/MachineSpecList-nio.txt")
-        .let { Files.readString(it, Charsets.UTF_8) }
-    println("okio output same as source? ${okioOutput.contentEquals(finalContent)}")
-    println("nio output same as source? ${nioOutput!!.contentEquals(finalContent)}")
 }
 
-fun encodeFromFile(file: Path) {
-
+fun encodeFromFile(raw: Path, edited: Path, destDir: Path) {
+    val filename = edited.name
+    FileSystem.SYSTEM.source(raw).buffer().use { rawSource->
+        var noiseBytes = getNoise(rawSource.readByteArray())
+        FileSystem.SYSTEM.source(edited).buffer().use{editedSource->
+            val editedLines = editedSource.readUtf8().split("\n")
+            for (line in editedLines) {
+                noiseBytes += line.toByteArray() + 0.toByte()
+            }
+            FileSystem.SYSTEM.sink("$destDir/$filename".toPath()).buffer().use {
+                it.write(noiseBytes)
+            }
+        }
+    }
 }
 
 private fun getNoise(bytes: ByteArray): ByteArray {
