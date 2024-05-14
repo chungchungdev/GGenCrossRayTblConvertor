8e9f6ad8312de8f7d5afa22a0cfda8e9dab10320
chungchungdev <chungchung.dev@gmail.com>
2024-05-14 08:30:39 +0800

    refactor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,38 @@ package com.chungchungdev
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.serialization.json.Json
 import okio.Path.Companion.toPath
+import kotlin.io.path.listDirectoryEntries
+import kotlin.io.path.nameWithoutExtension
 
 
 private val logger = KotlinLogging.logger { }
 
 fun main(args: Array<String>) {
-    oldRun()
+    val format = Json {
+        prettyPrint = true
+    }
+
+    val names = listOf(
+        "AbilitySpecList",
+        "CharacterSpecList",
+        "CreditList",
+        "DlcTitle",
+        "MachineSpecList",
+        "messagestring",
+        "MiscData",
+        "SeriesProfileList",
+        "SpecProfileList",
+        "StageList"
+    )
+
+    val rawFilesNames =
+        "src/main/data/raw".toPath().toNioPath()
+            .listDirectoryEntries("*.tbl")
+            .joinToString(",") { "\"${it.nameWithoutExtension}\"" }
+            .also { println(it) }
+
+    extract("CharacterSpecList",format)
+
 }
 
 fun oldRun() {
@@ -30,4 +56,20 @@ fun oldRun() {
     val packer = GGCRTextRepacker(logger)
     //packer.pack(editedPath, repackOutputDir, format)
     //packer.pack(decodedUnchanged, repackOutputDir, format)
+}
+
+fun extract(filenameNoExtension: String, format: Json) {
+    val rawPath = "src/main/data/raw/$filenameNoExtension.tbl".toPath()
+    val decodeOutputDir = "src/main/data/decoded/".toPath()
+
+    val extractor = GGCRTextExtractor(logger)
+    extractor.extractFile(rawPath, decodeOutputDir, format)
+}
+
+fun repack(filenameNoExtension: String, format: Json) {
+    val editedPath = "src/main/data/edited/$filenameNoExtension.json".toPath()
+    val repackOutputDir = "src/main/data/repack/".toPath()
+
+    val packer = GGCRTextRepacker(logger)
+    packer.pack(editedPath, repackOutputDir, format)
 }
